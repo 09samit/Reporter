63220437d7cc63ddafaff3098b0d5dcc50f0cadd
Amit Garg <09s.amitgarg@gmail.com>
2022-05-15 01:23:28 +0530

    Commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Reporter",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,8 +17,9 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(
-            name: "Reporter",
-            path: "./Sources/Backtrace_PLCrashReporter.xcframework"),
+        .binaryTarget(name: "Reporter",
+                              url: "https://github.com/09samit/Reporter/releases/tag/1.0.0/Backtrace_PLCrashReporter.xcframework.zip",
+                              checksum: "baf9fd33542b54e7dd4c01de3b327d7898ac25853095404f88dfec99605f76b9"),
+        .target(name: "Reporter", dependencies: ["Reporter"])
     ]
 )
